bddbdaf86b29c24a6f977fd6b8494397198c7ba9
LeslieWilson <leslie.wilson.vt@gmail.com>
2020-01-26 13:33:25 -0500

    got tasks to cross out sucessfully

--- src/components/FormDayOne.js
+++ src/components/FormDayOne.js
@@ -4,79 +4,80 @@ import TaskData from './TaskData.js'
 const FormDayOne = (props) =>{
 
 // theres going to be a field or multiple with a field and iscompleted variable, setting the state
-const [todoList, setTodoList] = useState([
-  {field:"",
-  isCompleted:false}
-])
+  const [todoList, setTodoList] = useState([
+    {field:"",
+    isCompleted:false}
+  ])
 
 // this is storing what is typed in state, last thing before you see it on page
-const [newTask, setNewTask] = useState({
-  field:""
-})
+  const [newTask, setNewTask] = useState({
+    field:""
+  })
 
 // this is setting whats typed to be stored in state, field:""
-const handleFieldChange = (event)=>{
-  setNewTask({
-    ...newTask,
-    [event.currentTarget.name]:
-    event.currentTarget.value
-  })
-}
+  const handleFieldChange = (event)=>{
+    setNewTask({
+      ...newTask,
+      [event.currentTarget.name]:
+      event.currentTarget.value
+    })
+  }
 
 // this is setting the payload to equel whatever was typed into the field in first part. payload is the connection between newtask and todolist because setting payload to newtask.field. settodolist is adding the payload to todolist, then setNewTask clears the field. accessing the newtask hash and then .field gives you the value of the hash
 
-const handleContactSubmit = (event) =>{
-  event.preventDefault()
-  let payload = {
-  field:newTask.field
+  const handleContactSubmit = (event) =>{
+    event.preventDefault()
+    let payload = {
+      field:newTask.field,
+      isCompleted:false
+    }
+    setTodoList([
+      ...todoList,
+      payload
+    ])
+
+    setNewTask({
+      field:""
+    })
   }
 
-  setTodoList([
-    ...todoList,
-    payload
-  ])
+// im mapping through todoList and this makes a new set of data, im setting it to tasklist. im mapping through and getting a task and index for each task and Im returning the component taskdata. < /> is how you create the html. this function is returning multiples of taskdata with the information inside the carrots and saving it to tasklist which you are rendering below. thats why you're not just rendering <TaskData />, because you need to render multiples. map is spreading out whats already there. Taskdata passing props. the reason you need field = {task.field} even though task should already be passing this, is because todolist is an array of hashes and you still need to access each value by mapping through the array of hashes and calling the keys
 
-  setNewTask({
-    field:""
+  let taskList = todoList.map((task, i) =>{
+
+    function toggleTodoCompleteAtIndex(){
+      const tempTodos = [...todoList]
+      tempTodos[i].isCompleted =!tempTodos[i].isCompleted
+      setTodoList(tempTodos)
+    }
+
+    return(
+      <TaskData
+      field={task.field}
+      isCompleted = {task.isCompleted}
+      onClick = {toggleTodoCompleteAtIndex}
+      />
+    )
   })
-}
 
 
-// im mapping through todoList and this makes a new set of data, im setting it to tasklist. im mapping through and getting a task and index for each task and Im returning the component taskdata. < /> is how you create the html. this function is returning multiples of taskdata with the information inside the carrots and saving it to tasklist which you are rendering below. thats why you're not just rendering <TaskData />, because you need to render multiples. map is spreading out whats already there. Taskdata is getting set to have props. Task.field is coming from, todolist is getting mapped thru and each task has two key value pairs and you're accessing field.
 
-function toggleTodoCompleteAtIndex(index) {
-    const temporaryTodos = [...todoList];
-    temporaryTodos[index].isCompleted = !temporaryTodos[index].isCompleted;
-    setTodoList(temporaryTodos);
-  }
-
-let taskList = todoList.map((task, i) =>{
   return(
-    <TaskData
-    field={task.field}
-    isCompleted = {task.isCompleted}
-    onClick = {()=>toggleTodoCompleteAtIndex(i)}
-    />
-  )
-})
-
-
- return (
    <div className = "form-left">
     <h2 className = "form-left-title">Day One</h2>
-      <form onSubmit ={handleContactSubmit}>
-         <label>
-               <input
-               name="field"
-               onChange = {handleFieldChange}
-               value= {newTask.field}
-               className="form_left_field"
-               placeholder="task"
-               />
-          </label>
-      </form>
-      {taskList}
-   </div>
- )
+    <form onSubmit ={handleContactSubmit}>
+     <label>
+       <input
+         name="field"
+         onChange = {handleFieldChange}
+         value= {newTask.field}
+         className="form_left_field"
+         placeholder="task"
+       />
+      </label>
+    </form>
+    {taskList}
+  </div>
+  )
 }
 export default FormDayOne
--- src/components/TaskData.js
+++ src/components/TaskData.js
@@ -1,33 +1,19 @@
 import React from 'react'
 
 const TaskData = props =>{
+let theTask
     if(props.isCompleted === true){
-      let theTask = "crossedOut"
+      theTask = "crossedOut"
     }else{
-      let theTask = "notCrossedOut"
+      theTask = "notCrossedOut"
     }
-    }
+
   return(
-    <div className={theTask} onClick ={props.onClick}>
-    <p>{props.field}</p>
+    <div onClick ={props.onClick}>
+    <p className={theTask}>{props.field}</p>
     </div>
-  </div>
+
   )
 }
 
 export default TaskData
-
-
-onClick={() => toggleTodoCompleteAtIndex(i)}
-
-so to change that one particular thing immidiatly in their browser you give it state and its stored temporarily in their browser and the information is at the same time sent to the server via a fetch call and the server is updated with this new information so when you return, componentdidmount will fire and get this information from the server and put it back into state and this is how it has persisted. instead of componentdidmount we are using useeffect.
-
-the component didmount also saves that same thing to the server at the sametime?
-
-i think i like reading it better yeah can you type what lifecycle methods do
-
-im just talking about componentDidMount -
-It is commonly used to fetch whatever data is stored on t he server to populate state whenever a page or component is loaded - then your handlesubmit will both update the state and send it to your server, then when the page refreshes or is browsed away from and back to - componentDidMount will fire again and get the information stored on the server to populate state ahgain
-so like once a react app is pushed up, when state cchanges, it changes on the browser of whoever is viewing it, and its saved in THEIR temporary memory in their browser but if i wanted the state not to go away on page refresh, and you'd use rails so it woulden't go away on page refresh. state will ALWAYS go away on page refresh but you have event things that happen on pageload. componentDidMount
-
-can you just type that here so i can read it instead of ...hearing you - code runs on their browser - this is saved in temporary memory in their browser - goes away on page refresh - NO SERVER
