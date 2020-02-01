da4be2f96039d1ea319f92a4d4210c5acf74e301
LeslieWilson <leslie.wilson.vt@gmail.com>
2020-02-01 14:14:03 -0500

    got task to persist in form one

--- src/components/FormDayOne.js
+++ src/components/FormDayOne.js
@@ -3,18 +3,18 @@ import TaskData from './TaskData.js'
 
 const FormDayOne = (props) =>{
 
-// theres going to be a field or multiple with a field and iscompleted variable, setting the state
+// making a placeholder for something called field and something called iscompleted to be one entry in a stateful todolist. whenever anything in the list gets changed you'd rerender the page with usestate.
   const [todoList, setTodoList] = useState([
     {field:"",
     isCompleted:false}
   ])
 
-// this is storing what is typed in state, last thing before you see it on page
+// this is storing what is typed in the field as you set a new task, whenever anything gets typed in the field you change the state
   const [newTask, setNewTask] = useState({
     field:""
   })
 
-// this is setting whats typed to be stored in state, field:""
+// this is setting whats typed to be stored in newTask (which is a dictionary with field:"")
   const handleFieldChange = (event)=>{
     setNewTask({
       ...newTask,
@@ -23,7 +23,11 @@ const FormDayOne = (props) =>{
     })
   }
 
-// this is setting the payload to equel whatever was typed into the field in first part. payload is the connection between newtask and todolist because setting payload to newtask.field. settodolist is adding the payload to todolist, then setNewTask clears the field. accessing the newtask hash and then .field gives you the value of the hash
+// you're making a new entry in todolist and iscomplete starts as false, field is whatever is stored in newtask. you're setting it all to payload
+
+ // settodolist is adding the payload to todolist using object destructuring because todolist is an array of hashes
+
+ // setNewTask clears the field
 
   const handleContactSubmit = (event) =>{
     event.preventDefault()
@@ -36,10 +40,41 @@ const FormDayOne = (props) =>{
       payload
     ])
 
+fetch("/api/v1/todos", {
+  method: "POST",
+  body: JSON.stringify(payload),
+  headers:{
+    Accept:"application/json",
+    "content-type": "application/json"
+  }
+})
+.then(response => {
+  if(response.ok){
+    return response;
+  }else{
+    const errorMessage =
+    `${response.status}
+    (${response.statusText})`
+    const error = new Error(errorMessage)
+    throw(error)
+  }
+})
+
+.then(response => response.json())
+.then(body=>{
+setTodoList([...body])
+})
+
+.catch((error) =>{console.error("error in fetch!")
+})
     setNewTask({
       field:""
     })
   }
+
+ // I defined this api in my rails server and upon pageload is fetching from it using get method, its receiving right now a list with one item in it.
+ //
+ // headers are indicating what it will accept
 
   useEffect(()=>{
     fetch("/api/v1/todos", {
@@ -58,16 +93,30 @@ const FormDayOne = (props) =>{
         throw(error);
       }
     })
+
+    //
+    // this is turning the information you sucessfully got from a response object into a json object. setting it to variable body, setting body to be a json string. setting todolist to be whatever info is because info is already an array of hashes.
+
     .then(response => response.json())
     .then(body=>{
-      let info = JSON.stringify(body)
+      console.log(body)
       setTodoList([
-        info
-      ])
+      ...body
+    ])
+     console.log(todoList)
+
+
     })
     .catch((error)=> {console.error("error in fetch")
   })
-})
+}, [])
+
+
+
+//
+// mapping through whatever is in todo list and adding an index, setting whatever is in todolist to a variable temp todolist. looking at an item at index[i], looking at the iscomplete status which starts as false, setting it equel to the opposit which would be true. if already true setting it to false. setting todolist to whatever temptodos equels.
+
+// you cant use the getter to change the variables stored in state. todolist just gets the items, you cant say  todolist.iscompleted = false. you need to copy it, change the copy, use setter to set whatever new value is.
 
   let taskList = todoList.map((task, i) =>{
     function toggleTodoCompleteAtIndex(){
